a46b699c4ed19815352c86fb97be416b429ce8eb
Aravind SV <arvind.sv@gmail.com>
2016-02-09 07:59:06 -0500

    Works with httpclient 4.5.1

--- TestConnection.java
+++ TestConnection.java
@@ -1,25 +1,20 @@
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.params.HttpConnectionParams;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 
 public class TestConnection {
-    public static void main(String[] args) {
-        HttpClient client = new HttpClient();
-        client.getParams().setIntParameter(HttpConnectionParams.CONNECTION_TIMEOUT, 10 * 1000);
-        client.getParams().setSoTimeout(5 * 60 * 1000);
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
 
-        GetMethod method = new GetMethod("https://dl.go.cd/experimental/repodata/repomd.xml");
-        method.setFollowRedirects(false);
         try {
-            int returnCode = client.executeMethod(method);
-            if (returnCode != HttpStatus.SC_OK) {
-                throw new RuntimeException(method.getStatusLine().toString());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            HttpGet httpget = new HttpGet("https://dl.go.cd/experimental/repodata/repomd.xml");
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            System.err.println(response);
+        } finally {
+            httpclient.close();
         }
     }
 }
